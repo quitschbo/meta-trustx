quitschbo/meta-trustx

commit d8b99868876a38315b6779db5473a27558220698

recipes-tss/openssl-tpm2-engine: updated to version 4.3.0
update openssl-tpm2-engine to version 4.3.0 to be able to
use the openssl 3 provider API instead of the
deprecated engine API

Signed-off-by: Simon Ott <simon.ott@aisec.fraunhofer.de>

recipes-tss/openssl-tpm2-engine/openssl-tpm2-engine_4.3.0.bb
@@ -1,0 +1,49 @@
+SUMMARY = "TPM2 engine and key creation tools for openssl"
+DESCRIPTION = "This package contains 2 sets of code, a command-line utility used to \
+generate a TSS key blob and write it to disk and an OpenSSL engine which \
+interfaces with the TSS API.  Because this application uses the TPM \
+cryptographically, it must be build with the IBM TSS."
+
+HOMEPAGE = "https://git.kernel.org/pub/scm/linux/kernel/git/jejb/openssl_tpm2_engine.git/"
+LICENSE = "LGPL-2.1-only"
+
+DEPENDS = "openssl ibmtss2"
+
+RDEPENDS:${PN} += "openssl libgcc"
+
+EXTRA_OECONF = "--with-openssl=${RECIPE_SYSROOT}/usr"
+
+TAR_N = "openssl_tpm2_engine"
+
+LIC_FILES_CHKSUM = "file://LICENSE;md5=a055871bc591288e6970672b3ff8736d"
+SRC_URI = "https://git.kernel.org/pub/scm/linux/kernel/git/jejb/${TAR_N}.git/snapshot/${TAR_N}-${PV}.tar.gz \
+	file://Cross-compile-compatible-enginesdir-variable_${PV}.patch \
+	file://Makefile.am-Use-src_topdir-instead-of-relative-inclu_${PV}.patch \
+	file://src-provider-keymgmt-initialize-order-in-tpm2_keymgm_${PV}.patch \
+	file://0001-fix-uninitialized-variables-reported-by-gcc_${PV}.patch \
+	file://Do-not-use-deprecated-ibmtss-functions_${PV}.patch \
+"
+
+S = "${WORKDIR}/${TAR_N}-${PV}"
+
+EXTRA_OEMAKE = "-I ${S}/src/include"
+
+SRC_URI[md5sum] = "ada62829fda2d98223580e969422e3ab"
+SRC_URI[sha256sum] = "e3ed1d80644eb2731541a787adc6d9c0440918c21a1079514b20529594c4cbab"
+
+inherit autotools pkgconfig
+
+do_configure:prepend() {
+	touch ${S}/NEWS
+	touch ${S}/AUTHORS
+	touch ${S}/ChangeLog
+	cp ${S}/LICENSE ${S}/COPYING
+}
+
+do_install:append() {
+	rm -rf ${D}${libdir}/engines
+	rm -f ${D}${libdir}/ossl-modules/tpm2.so
+}
+
+FILES:${PN} += "${libdir}/*"
+FILES:${PN} += "${bindir}/*"

recipes-tss/openssl-tpm2-engine/openssl-tpm2-engine/0001-fix-uninitialized-variables-reported-by-gcc_4.3.0.patch
@@ -1,0 +1,47 @@
+diff --git a/src/libcommon/tpm2-common.c b/src/libcommon/tpm2-common.c
+index be3fe50..edfc70e 100644
+--- a/src/libcommon/tpm2-common.c
++++ b/src/libcommon/tpm2-common.c
+@@ -687,7 +687,7 @@ TPM_RC tpm2_ObjectPublic_GetName(NAME_2B *name,
+ 	TPM_RC rc = 0;
+ 	uint16_t written = 0;
+ 	TPMT_HA digest;
+-	uint32_t sizeInBytes;
++	uint32_t sizeInBytes = 0;
+ 	uint8_t buffer[MAX_RESPONSE_SIZE];
+ 
+ 	/* marshal the TPMT_PUBLIC */
+@@ -1282,7 +1282,7 @@ TPM_RC tpm2_init_session(TSS_CONTEXT *tssContext, TPM_HANDLE handle,
+ 	int num_commands;
+ 	struct policy_command *commands;
+ 	char prefix[128];
+-	TPM_RC rc;
++	TPM_RC rc = TPM_RC_POLICY;
+ 	TPM_ALG_ID name_alg = app_data->Public.publicArea.nameAlg;
+ 
+ 	*auth = app_data->auth;
+diff --git a/src/provider/provider.c b/src/provider/provider.c
+index f913bc6..d68034b 100644
+--- a/src/provider/provider.c
++++ b/src/provider/provider.c
+@@ -100,7 +100,7 @@ int OSSL_provider_init(const OSSL_CORE_HANDLE *handle,
+ 		       const OSSL_DISPATCH **out,
+ 		       void **provctx)
+ {
+-	OSSL_LIB_CTX *libctx;
++	OSSL_LIB_CTX *libctx = NULL;
+ 	const OSSL_DISPATCH *fns = in;
+ 	int i;
+ 	OSSL_PARAM provider_params[] = {
+diff --git a/src/tools/seal_tpm2_data.c b/src/tools/seal_tpm2_data.c
+index e7eb52a..9903d5e 100644
+--- a/src/tools/seal_tpm2_data.c
++++ b/src/tools/seal_tpm2_data.c
+@@ -392,6 +392,7 @@ int main(int argc, char **argv)
+ 			if (!parent) {
+ 				fprintf(stderr, "Unknown parent '%s'\n",
+ 					parent_str);
++				rc = NOT_TPM_ERROR;
+ 				goto out_flush;
+ 			}
+ 		} else {

recipes-tss/openssl-tpm2-engine/openssl-tpm2-engine/Cross-compile-compatible-enginesdir-variable_4.3.0.patch
@@ -1,0 +1,88 @@
+From d04f8e55793fef8c43718b2968cb9bd877cd57eb Mon Sep 17 00:00:00 2001
+From: =?UTF-8?q?Michael=20Wei=C3=9F?= <michael.weiss@aisec.fraunhofer.de>
+Date: Wed, 9 Aug 2023 14:23:25 +0200
+Subject: [PATCH] Cross-compile compatible enginesdir variable
+
+Get enginesdir from pkg-config instead of non portable use of gcc.
+Further provide a --with-engingesdir option for configure.
+---
+ configure.ac           | 42 ++++++++++++++----------------------------
+ src/engine/Makefile.am |  2 +-
+ 2 files changed, 15 insertions(+), 29 deletions(-)
+
+diff --git a/configure.ac b/configure.ac
+index 9e74274..3423aa7 100644
+--- a/configure.ac
++++ b/configure.ac
+@@ -55,41 +55,27 @@ PKG_CHECK_MODULES([DEPS], [libcrypto >= 3.0.0],
+                   [ac_have_openssl3=1],
+ 		  [PKG_CHECK_MODULES([DEPS], [libcrypto])])
+ 
+-AC_ARG_WITH(
+-	[enginesdir],
+-	[AC_HELP_STRING([--with-enginesdir], [OpenSSL engines directory])],
+-	[enginesdir="${withval}"],
+-	[AC_LANG_CONFTEST([AC_LANG_PROGRAM(
+-		[[
+-		  #define HEADER_CRYPTLIB_H
+-		  #include <openssl/crypto.h>
+-		  #include <stdio.h>
+-		]],
+-		[[
+-		  #if OPENSSL_VERSION_NUMBER < 0x10100000
+-		  puts(ENGINESDIR);
+-		  #else
+-		  puts(OpenSSL_version(OPENSSL_ENGINES_DIR));
+-		  #endif
+-		]]
+-		)])
+-		gcc $CFLAGS conftest.c -lcrypto
+-		enginesdir=`./a.out|sed 's/ENGINESDIR: //'`
+-	]
+-	)
+-
+-if test -z "$enginesdir" ; then
+-    AC_MSG_FAILURE([Failed to find SSL engines directory])
+-fi
++AC_ARG_WITH([enginesdir],
++	[AS_HELP_STRING([--with-enginesdir],
++		[Set the OpenSSL engine directory (default: use pkg-config)])],
++	[],
++	[with_enginesdir=$ac_enginesdir])
++AS_IF([test -z "$with_enginesdir"],
++	[AC_MSG_WARN([Empty enginesdir, using $libdir/engines instead.])])
++# This weirdness is necessary to enable distcheck via DISTCHECK_CONFIGURE_FLAGS
++AS_IF([test -z "$with_enginesdir"],
++	[with_enginesdir=$libdir/engines])
+ 
+ if test "$ac_have_openssl3" = "1"; then
+     modulesdir=\"`$PKG_CONFIG --variable=modulesdir libcrypto`\"
+     AC_SUBST(modulesdir)
+ fi
+ 
+-AC_SUBST(enginesdir)
++AC_SUBST(ENGINESDIR, "$with_enginesdir")
++
++PKG_CHECK_MODULES([DEPS], [libcrypto],
++	[ac_enginesdir=`$PKG_CONFIG --variable=enginesdir libcrypto`])
+ 
+-PKG_CHECK_MODULES([DEPS], [libcrypto])
+ 
+ AC_SEARCH_LIBS([TSS_Create], [tss ibmtss], [
+ 	AC_DEFINE(HAVE_IBM_TSS, 1, [Defined if we have the IBM TSS])
+diff --git a/src/engine/Makefile.am b/src/engine/Makefile.am
+index 9ce2a25..a2a0cad 100644
+--- a/src/engine/Makefile.am
++++ b/src/engine/Makefile.am
+@@ -1,7 +1,7 @@
+ AM_CPPFLAGS = -I ../include ${DEPRECATION}
+ 
+ openssl_engine_LTLIBRARIES=libtpm2.la
+-openssl_enginedir=@enginesdir@
++openssl_enginedir=$(ENGINESDIR)
+ 
+ COMMONLIB = ../libcommon/libcommon.a
+ 
+-- 
+2.30.2
+

recipes-tss/openssl-tpm2-engine/openssl-tpm2-engine/Makefile.am-Use-src_topdir-instead-of-relative-inclu_4.3.0.patch
@@ -1,0 +1,57 @@
+diff --git a/src/engine/Makefile.am b/src/engine/Makefile.am
+index 9ce2a25..af92c02 100644
+--- a/src/engine/Makefile.am
++++ b/src/engine/Makefile.am
+@@ -1,4 +1,4 @@
+-AM_CPPFLAGS = -I ../include ${DEPRECATION}
++AM_CPPFLAGS = -I $(top_srcdir)/src/include ${DEPRECATION}
+ 
+ openssl_engine_LTLIBRARIES=libtpm2.la
+ openssl_enginedir=@enginesdir@
+diff --git a/src/libcommon/Makefile.am b/src/libcommon/Makefile.am
+index ad566ff..1c88e37 100644
+--- a/src/libcommon/Makefile.am
++++ b/src/libcommon/Makefile.am
+@@ -1,6 +1,6 @@
+ noinst_LIBRARIES = libcommon.a
+ 
+-AM_CPPFLAGS = -I ../include/ ${DEPRECATION}
++AM_CPPFLAGS = -I $(top_srcdir)/src/include ${DEPRECATION}
+ 
+ libcommon_a_SOURCES = tpm2-common.c
+ libcommon_a_CFLAGS = -fPIC
+diff --git a/src/opensslmissing/Makefile.am b/src/opensslmissing/Makefile.am
+index bc4243a..a829d4a 100644
+--- a/src/opensslmissing/Makefile.am
++++ b/src/opensslmissing/Makefile.am
+@@ -1,6 +1,6 @@
+ noinst_LIBRARIES = libosslm.a
+ 
+-AM_CPPFLAGS = -I ../include
++AM_CPPFLAGS = -I $(top_srcdir)/src/include
+ 
+ if HAVE_OPENSSL3
+ libosslm_a_SOURCES = rsa.c signature.c
+diff --git a/src/provider/Makefile.am b/src/provider/Makefile.am
+index 5ff91cf..ca6966c 100644
+--- a/src/provider/Makefile.am
++++ b/src/provider/Makefile.am
+@@ -1,4 +1,4 @@
+-AM_CPPFLAGS=-I../include
++AM_CPPFLAGS = -I $(top_srcdir)/src/include
+ COMMONLIB = ../libcommon/libcommon.a ../opensslmissing/libosslm.a
+ 
+ if HAVE_OPENSSL3
+diff --git a/src/tools/Makefile.am b/src/tools/Makefile.am
+index 3012411..b64ceb9 100644
+--- a/src/tools/Makefile.am
++++ b/src/tools/Makefile.am
+@@ -10,7 +10,7 @@ man7_MANS = openssl_tpm2_engine.7
+ CLEANFILES = $(man1_MANS)
+ endif
+ 
+-AM_CPPFLAGS = -I ../include ${DEPRECATION}
++AM_CPPFLAGS = -I $(top_srcdir)/src/include ${DEPRECATION}
+ 
+ bin_PROGRAMS=create_tpm2_key load_tpm2_key seal_tpm2_data unseal_tpm2_data \
+ 	signed_tpm2_policy attest_tpm2_primary

recipes-tss/openssl-tpm2-engine/openssl-tpm2-engine/src-provider-keymgmt-initialize-order-in-tpm2_keymgm_4.3.0.patch
@@ -1,0 +1,26 @@
+From fab9a93bc950983fb724355e60ce5418cafdd520 Mon Sep 17 00:00:00 2001
+From: =?UTF-8?q?Michael=20Wei=C3=9F?= <michael.weiss@aisec.fraunhofer.de>
+Date: Thu, 10 Aug 2023 13:08:10 +0200
+Subject: [PATCH] src/provider/keymgmt: initialize order in
+ tpm2_keymgmt_import()
+
+---
+ src/provider/keymgmt.c | 2 +-
+ 1 file changed, 1 insertion(+), 1 deletion(-)
+
+diff --git a/src/provider/keymgmt.c b/src/provider/keymgmt.c
+index aa9c120..31929da 100644
+--- a/src/provider/keymgmt.c
++++ b/src/provider/keymgmt.c
+@@ -177,7 +177,7 @@ static int tpm2_keymgmt_import(void *key, int selection,
+ 	EC_POINT *pt;
+ 	BIGNUM *x, *y;
+ 	TPMS_ECC_POINT *tpt = &ad->Public.publicArea.unique.ecc;
+-	int order;
++	int order = 0;
+ 	int ret = 1;
+ 
+ 	if ((selection & OSSL_KEYMGMT_SELECT_PUBLIC_KEY) == 0)
+-- 
+2.30.2
+

commit be41c2491d8e5bf03f7a97dd67fb3edc7eefd311

recipes-tss/openssl-tpm2-engine: added 32bit compat patch
Recent update to 4.3.0 did not compile on 32bit arch. Thus, added
backport of upstream commit 508060a88b6e ("Fix 32 bit errors").

Fixes: d8b99868876a ("recipes-tss/openssl-tpm2-engine: updated to version 4.3.0")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

recipes-tss/openssl-tpm2-engine/openssl-tpm2-engine/Fix-32-bit-errors_4.3.0.patch
@@ -1,0 +1,32 @@
+From 508060a88b6e928b534a53bc0363d1017babc94e Mon Sep 17 00:00:00 2001
+From: James Bottomley <James.Bottomley@HansenPartnership.com>
+Date: Fri, 1 Nov 2024 17:49:22 -0500
+Subject: [PATCH] Fix 32 bit errors
+
+One formatting problem with UINT64 and a must check annotation on
+read that only seems to exist in the 32 bit builds.
+
+Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>
+---
+ src/tools/attest_tpm2_primary.c | 5 +++--
+ 1 file changed, 3 insertions(+), 2 deletions(-)
+
+diff --git a/src/tools/attest_tpm2_primary.c b/src/tools/attest_tpm2_primary.c
+index 3acd511..8b14f6b 100644
+--- a/src/tools/attest_tpm2_primary.c
++++ b/src/tools/attest_tpm2_primary.c
+@@ -416,8 +416,9 @@ void do_certify(const char *auth, TPM_HANDLE handle, const char *namefile,
+ 		tpm2_get_hexname(hexname, &pubh);
+ 		printf("%s\n", hexname);
+ 	} else {
+-		printf("Good certification from TPM at %lu.%04d reset count %u\n",
+-		       a.clockInfo.clock/1000, (int)(a.clockInfo.clock%1000),
++		printf("Good certification from TPM at %llu.%04d reset count %u\n",
++		       (unsigned long long)a.clockInfo.clock/1000,
++		       (int)(a.clockInfo.clock%1000),
+ 		       a.clockInfo.resetCount);
+ 	}
+ 	rc = 0;
+-- 
+2.39.5
+

recipes-tss/openssl-tpm2-engine/openssl-tpm2-engine_4.3.0.bb
@@ -22,6 +22,7 @@ SRC_URI = "https://git.kernel.org/pub/scm/linux/kernel/git/jejb/${TAR_N}.git/sna
 	file://src-provider-keymgmt-initialize-order-in-tpm2_keymgm_${PV}.patch \
 	file://0001-fix-uninitialized-variables-reported-by-gcc_${PV}.patch \
 	file://Do-not-use-deprecated-ibmtss-functions_${PV}.patch \
+	file://Fix-32-bit-errors_${PV}.patch \
 "
 
 S = "${WORKDIR}/${TAR_N}-${PV}"